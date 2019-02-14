4f77e91a9add5203ac7cab778cc79ada25d4ebaf
juho <krooh@hotmail.com>
2019-02-14 10:01:00 +0200

    osa2 ready

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 import Persons from './components/Persons'
 import AddNewPersonForm from './components/AddNewPersonForm'
 import FilterForm from './components/FilterForm'
-
+import personService from './services/persons'
+import Notification from './components/Notification'
+import Error from './components/Error'
 const App = () => {
     const [persons, setPersons] = useState([])
     useEffect(() => {
@@ -17,6 +19,8 @@ const App = () => {
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [newFilter, setNewFilter] = useState('')
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [notificationMessage, setNotificationMessage] = useState(null)
 
     const handleNumberChange = (event) => {
         setNewNumber(event.target.value)
@@ -27,26 +31,89 @@ const App = () => {
     const handleFilterChange = (event) => {
         setNewFilter(event.target.value)
     }
+    const showNotification = (message) => {
+        setNotificationMessage(message)
+        setTimeout(() => {
+            setNotificationMessage(null)
+        }, 7000)
+    }
+    const showError = (message) => {
+        setErrorMessage(message)
+        setTimeout(() => {
+            setErrorMessage(null)
+        }, 7000)
+    }
     const addPerson = (event) => {
         event.preventDefault()
-        const names = persons.map(person => person.name)
-        if (names.indexOf(newName) >= 0) {
-            window.alert(newName + ' on jo puhelinluettelossa')
-            return
-        }
         const personObject = {
             name: newName,
             number: newNumber,
         }
+        const names = persons.map(person => person.name)
+        const index = names.indexOf(newName)
+        if (index >= 0) {
+            if (!window.confirm(newName +
+                ' on jo luettelossa. Korvataanko vanha numero uudella?')) {
+                return
+            }
+            const id = persons[index].id
+            personService
+                .update(id, personObject)
+                .then(returnedPerson => {
+                    let newPersons = persons
+                    newPersons[index] = returnedPerson
+                    // setPersons(persons.filter(n => n.id !== id).concat(returnedPerson))
+                    setPersons(newPersons)
+                    showNotification('Korvattiin henkilö ' + newName
+                        + ' numero uudella numerolla ' + newNumber)
+                    setNewName('')
+                    setNewNumber('')
+                })
+                .catch(error => {
+                    showError(`Henkilö '${newName}' oli jo poistettu palvelimelta`)
+                    setPersons(persons.filter(n => n.id !== id))
+                })
+            return
+        }
 
-        setPersons(persons.concat(personObject))
-        setNewName('')
-        setNewNumber('')
+
+        personService
+            .create(personObject)
+            .then(returnedPerson => {
+                setPersons(persons.concat(returnedPerson))
+                showNotification('Lisättiin ' + newName)
+                setNewName('')
+                setNewNumber('')
+
+            })
+    }
+
+    const removePerson = (id) => {
+        return () => {
+            const name = persons.filter(n => n.id === id)[0].name
+            if (!window.confirm('Poistetaanko ' + name)) {
+                return
+            }
+            console.log('remove person with id ', id)
+            personService
+                .remove(id)
+                .then(returnedPerson => {
+                    console.log('person removed')
+                    showNotification(name + ' poistettiin')
+                    setPersons(persons.filter(n => n.id !== id))
+                })
+                .catch(error => {
+                    showError(`Henkilö '${name}' oli jo poistettu palvelimelta`)
+                    setPersons(persons.filter(n => n.id !== id))
+                })
+        }
     }
     const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
     return (
         <div>
             <h1>Puhelinluettelo</h1>
+            <Notification message={notificationMessage} />
+            <Error message={errorMessage} />
             <FilterForm handleFilterChange={handleFilterChange}
                 newFilter={newFilter} />
             <AddNewPersonForm handleNameChange={handleNameChange}
@@ -54,7 +121,9 @@ const App = () => {
                 newName={newName}
                 newNumber={newNumber}
                 addPerson={addPerson} />
-            <Persons persons={filteredPersons} />
+            <Persons
+                persons={filteredPersons}
+                removePerson={removePerson} />
         </div>
     )
 
--- puhelinluettelo/src/components/Persons.js
+++ puhelinluettelo/src/components/Persons.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from './Header'
 import Person from './Person'
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, removePerson }) => {
     return (
         <div>
             <Header title='Numerot' />
             {persons.map(person => <Person person={person}
-                key={person.name + person.number} />
+                key={person.id}
+                removePerson={removePerson(person.id)} />
             )}
         </div>
     )
